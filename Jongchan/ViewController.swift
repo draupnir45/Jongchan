b34a0cffadded61df4e37267586f5654803ed85e
Jayden <jayden.park@additor.io>
2019-08-12 13:36:32 +0900

    Initial Commit

---
+++ Jongchan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jongchan
+//
+//  Created by 박종찬 on 12/08/2019.
+//  Copyright © 2019 박종찬. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
