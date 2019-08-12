8d3c7882145f99d6919f749de33e828a7876b3aa
Jayden <jayden.park@additor.io>
2019-08-12 13:53:58 +0900

    Simulate 'fail' for test.

---
+++ JongchanTests/JongchanTests.swift
@@ -0,0 +1,17 @@
+//
+//  JongchanTests.swift
+//  JongchanTests
+//
+//  Created by 박종찬 on 12/08/2019.
+//  Copyright © 2019 박종찬. All rights reserved.
+//
+
+import XCTest
+
+class JongchanTests: XCTestCase {
+
+    func testExample() {
+        XCTAssert(1 + 1 != 2)
+    }
+
+}
